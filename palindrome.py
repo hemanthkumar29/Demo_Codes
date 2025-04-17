d1a7808b03008d06750c66551782384137797242
Hemanth Kumar <161832203+hemanthkumar29@users.noreply.github.com>
2025-04-17 10:24:16 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,6 @@
+num = int(input("Enter the number you want to check : "))
+rev = input("Enter the Number in reverse : ")
+if (num == rev):
+    print("It is a palindrome number.")
+else:
+    print("It is not a palindrome.")
